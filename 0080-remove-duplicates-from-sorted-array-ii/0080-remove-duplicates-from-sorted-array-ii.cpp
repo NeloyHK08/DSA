3ae2dbff87d039fa37deedd12537db196234f5e6
NeloyHK08 <144938696+NeloyHK08@users.noreply.github.com>
2026-02-07 23:18:21 +0530

    Time: 3 ms (95.65%), Space: 19.6 MB (23.21%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,7 +4,7 @@ public:
         int l = 0, r = 0;
         while(r < nums.size()){
             int count = 1;
-            while(r+1<nums.size() && nums[r] == nums[r+1]){
+            while((r+1)<nums.size() && nums[r] == nums[r+1]){
                 r++;
                 count++;
             }
