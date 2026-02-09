0faea61d4ebc5fe4d4cc2cd5a4f8205210ef0a67
NeloyHK08 <144938696+NeloyHK08@users.noreply.github.com>
2026-02-09 11:26:38 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.89%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long pow = n;
+        double ans = 1.0;
+
+        if(pow < 0){
+            x = 1 / x;
+            pow = -pow;
+        }
+        
+
+        while(pow > 0){
+            if(pow % 2 == 1){
+                ans *= x;//odd
+            }
+
+            x *= x;
+            pow /= 2;
+            }
+        
+        return ans;
+    }
+};
